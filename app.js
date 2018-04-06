705dfcbf462fb6aada6f51f3e78a7a995fedd675
botnetdobbs <lazarusodhis@gmail.com>
2018-04-06 15:36:38 +0300

    It is working but comments don't hurt

--- app.js
+++ app.js
@@ -10,9 +10,11 @@ var converter = new Converter();
 converter.fromFile(path.join(__dirname, 'data', 'customer-data.csv'))
     .on('end_parsed', (result) => {
         var raw = JSON.stringify(result, undefined, 2);
+        //Call the function and give the required parameters
         writeToJsonFile(path.join(__dirname, 'data', 'customer-data.json'), raw);
     })
 
+//Write the output to JSON file
 function writeToJsonFile(file, data) {
     fs.writeFile(file, data, (err) => {
         if (err) {
