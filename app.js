74759c0e5f83843c32df34e0817d3d6ca27a480a
botnetdobbs <lazarusodhis@gmail.com>
2018-04-06 15:27:28 +0300

    Convert the csv file and output result on console

--- app.js
+++ app.js
@@ -3,3 +3,12 @@ const fs = require('fs');
 
 //Require the csvtojs Converter class
 const Converter = require('csvtojson').Converter;
+
+//Create a new converter object from the class
+var converter = new Converter();
+
+converter.fromFile(path.join(__dirname, 'data', 'customer-data.csv'))
+    .on('end_parsed', (result) => {
+        var raw = JSON.stringify(result, undefined, 2);
+        console.log(raw);
+    })
